a7242a4b1d7aa57085b72414a678ab6be8cf5ddb
Justin Pinner <justinpinner@gmail.com>
2019-06-01 20:14:00 +0100

    More npm/webpack head-scratching

--- release.sh
+++ release.sh
@@ -6,4 +6,5 @@ rm -rf dist
 echo 'building for release...'
 npm run build-prod
 echo 'done!'
-
+echo ''
+echo 'you can now run "npm publish [--tag beta] [--dry-run]"'
--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "Eccentric.[name].js",
+    filename: "[name].js",
     libraryTarget: "umd",
     library: ["EccentricEngine", "[name]"],
     umdNamedDefine: true
